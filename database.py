0c286828f5e91c532e6258561a7b72371c8f2e52
Runge, Jan <J.Runge@wertgarantie.com>
2021-06-06 18:55:15 +0200

    fis errors in database script

--- database.py
+++ database.py
@@ -5,13 +5,9 @@ def connect():
     path = "DB.sqlite"
     connection = None
     try:
-
         connection = sqlite3.connect(path)
-
         print("Connection to SQLite DB successful")
-
     except Error as e:
-
         print(f"The error '{e}' occurred")
     return connection
 
