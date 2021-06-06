de41e6366f08a438806ea17a3bbc658e54d0983a
Runge, Jan <J.Runge@wertgarantie.com>
2021-06-06 20:15:11 +0200

    Fix errors in Web

--- database.py
+++ database.py
@@ -6,7 +6,8 @@ def connect():
     connection = None
     try:
         connection = sqlite3.connect(path, detect_types=sqlite3.PARSE_DECLTYPES |
-                                                        sqlite3.PARSE_COLNAMES)
+                                                        sqlite3.PARSE_COLNAMES, 
+                                    check_same_thread=False)
         print("Connection to SQLite DB successful")
     except Error as e:
         print(f"The error '{e}' occurred")
--- web.py
+++ web.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from datetime import timezone
+from datetime import timezone, datetime
 import database
 
 db = database.connect()
@@ -10,12 +10,14 @@ def hello_world():
     data = get_data()
     val, ts = data[0]
     str = f"{val}, {ts}"
-    return "<p>Hello, World!</p>"
+    return str
 
 def get_data():
-    temp_data = database.select(db, "select * from temperature_log where timestamp < datetime('now','-2 day','localtime') order by timestamp asc")
+    temp_data = database.select(db, "select temperature, timestamp from temperature_log where timestamp > datetime('now','-2 day','localtime') order by timestamp asc")
     result = []
     for row in temp_data:
         val, timestamp = row
-        timestamp = timestamp.replace(tzinfo=timezone.utc).timestamp()
-        result.append((val, timestamp))
+        timestamp = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
+        timestamp_new = (timestamp - datetime(1970, 1, 1)).total_seconds()
+        result.append((val, timestamp_new))
+    return result
