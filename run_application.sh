952a4189123679acabdf37adca7dbd6de63f549f
Runge, Jan <J.Runge@wertgarantie.com>
2021-06-06 20:01:05 +0200

    shutdown server from startup file

--- run_application.sh
+++ run_application.sh
@@ -1,3 +1,20 @@
+#!/bin/bash
+#      ^^^^ - NOT /bin/sh, as this code uses arrays
+
+pids=( )
 export FLASK_APP=web
-flask run --host=0.0.0.0 & 
-python3 main.py
+
+# define cleanup function
+cleanup() {
+  for pid in "${pids[@]}"; do
+    kill -0 "$pid" && kill "$pid" # kill process only if it's still running
+  done
+}
+
+# and set that function to run before we exit, or specifically when we get a SIGTERM
+trap cleanup EXIT TERM
+
+flask run --host=0.0.0.0 &  pids+=( "$!" )
+python3 main.py & pids+=( "$!" )
+
+wait # sleep until all background processes have exited, or a trap fires
